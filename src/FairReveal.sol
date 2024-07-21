cbec3b1d19d8a4b0ad722463b9cacc7d2c362751
flick <github@0xflick.xyz>
2024-07-21 11:42:09 -0600

    more tests

--- src/FairReveal.sol
+++ src/FairReveal.sol
@@ -5,8 +5,12 @@ import {LibString} from "solady/utils/LibString.sol";
 import {LibBitmap} from "solady/utils/LibBitmap.sol";
 import {OwnableRoles} from "solady/auth/OwnableRoles.sol";
 import {ITokenURIGenerator} from "./ITokenURIGenerator.sol";
-import {IERC4906} from "./IERC4906.sol";
 import "forge-std/console.sol";
+
+interface ITokemEmitable {
+    function emitBatchMetadataUpdate(uint16 start, uint16 end) external;
+    function emitMetadataUpdate(uint16 tokenId) external;
+}
 
 /**
  * @title FairReveal
@@ -26,7 +30,7 @@ contract FairReveal is OwnableRoles, ITokenURIGenerator {
     string private _tokenIdSalt;
     string private _uri;
     string private _unresolvedUri;
-    IERC4906 public emitable;
+    ITokemEmitable public emitable;
 
     struct RevealedChunk {
         uint16 startIndex;
@@ -52,7 +56,8 @@ contract FairReveal is OwnableRoles, ITokenURIGenerator {
         string memory unresolvedUri,
         uint16 totalTokens
     ) {
-        emitable = IERC4906(emitableAddress);
+        highestTotalAvailableArt = 0;
+        emitable = ITokemEmitable(emitableAddress);
         _uri = uri;
         _unresolvedUri = unresolvedUri;
         unrevealedTokensRemaining = totalTokens;
@@ -65,43 +70,42 @@ contract FairReveal is OwnableRoles, ITokenURIGenerator {
     function resolveTokenId(
         uint256 index
     ) public view returns (uint256 tokenId, uint256 salt) {
-        console.log("resolveTokenId [index: %d]", index);
-        console.log("resolveTokenId [_revealed.length: %d]", _revealed.length);
+        // console.log("resolveTokenId [index: %d]", index);
+        // console.log("resolveTokenId [_revealed.length: %d]", _revealed.length);
         uint256 currentIndexOfTokenId = 0;
         // Walk through the revealed chunks to find the tokenId by counting lengths
         for (uint256 i = 0; i < _revealed.length; i++) {
-            console.log("resolveTokenId [i: %d]", i);
+            // console.log("resolveTokenId [i: %d]", i);
             Revealed memory revealed = _revealed[i];
             uint256 chunkLength = revealed.chunks.length;
-            console.log("resolveTokenId [chunkLength: %d]", chunkLength);
+            // console.log("resolveTokenId [chunkLength: %d]", chunkLength);
             for (uint256 j = 0; j < chunkLength; j += 2) {
-                console.log("resolveTokenId [j: %d]", j);
+                // console.log("resolveTokenId [j: %d]", j);
                 uint16 startIndex = revealed.chunks[j];
                 uint16 length = revealed.chunks[j + 1];
-                console.log(
-                    "resolveTokenId [startIndex: %d, length: %d]",
-                    startIndex,
-                    length
-                );
+                // console.log(
+                //     "resolveTokenId [startIndex: %d, length: %d]",
+                //     startIndex,
+                //     length
+                // );
                 if (
                     index >= currentIndexOfTokenId &&
                     index < currentIndexOfTokenId + length
                 ) {
-                    uint16 startIndex = revealed.chunks[j];
                     tokenId = startIndex + index - currentIndexOfTokenId;
                     salt = revealed.salt;
-                    console.log(
-                        "resolveTokenId [tokenId: %d, salt: %d]",
-                        tokenId,
-                        salt
-                    );
+                    // console.log(
+                    //     "resolveTokenId [tokenId: %d, salt: %d]",
+                    //     tokenId,
+                    //     salt
+                    // );
                     return (tokenId, salt);
                 }
                 currentIndexOfTokenId += length;
-                console.log(
-                    "resolveTokenId [currentIndexOfTokenId: %d]",
-                    currentIndexOfTokenId
-                );
+                // console.log(
+                //     "resolveTokenId [currentIndexOfTokenId: %d]",
+                //     currentIndexOfTokenId
+                // );
             }
         }
         revert InvalidTokenId();
@@ -125,24 +129,35 @@ contract FairReveal is OwnableRoles, ITokenURIGenerator {
 
     function findFirstSetNoRollover(
         uint16 startAtIndex,
-        uint16 length
+        uint16 length,
+        uint16 rollover
     ) internal view returns (uint16) {
         uint16 index = startAtIndex;
-
+        console.log(
+            "findFirstSetNoRollover:[length: %d, rollover: %d]",
+            length,
+            rollover
+        );
         while (
             !_revealedTokens.get(index) &&
-            index < startingSize &&
+            index < rollover &&
             index - startAtIndex < length
         ) {
             index++;
         }
+        console.log(
+            "[findFirstSetNoRollover: %d, startAtIndex: %d]",
+            index,
+            startAtIndex
+        );
         return index;
     }
 
     error AllBitsAreSet();
     error OutOfRange();
     function findFirstUnset(
-        uint16 startAtIndex
+        uint16 startAtIndex,
+        uint16 rollover
     ) internal view returns (uint16) {
         if (startAtIndex >= startingSize) {
             revert OutOfRange();
@@ -153,19 +168,88 @@ contract FairReveal is OwnableRoles, ITokenURIGenerator {
             index++;
             checked++;
             // Roll over the index back to zero if it reaches or exceeds startingSize
-            if (index >= startingSize) {
+            if (index >= rollover) {
                 index = 0;
             }
         }
         // If all bits are set, this condition will prevent an infinite loop
-        if (checked >= startingSize) {
+        if (checked >= rollover) {
             revert AllBitsAreSet();
         }
         return index;
     }
 
+    error NotEnoughUnsetBits();
+    function findFirstUnsetCounting(
+        uint16 startAtIndex,
+        uint16 count,
+        uint16 rollover
+    ) internal view returns (uint16) {
+        if (startAtIndex >= rollover) {
+            revert OutOfRange();
+        }
+        console.log(
+            "findFirstUnsetCounting [startAtIndex: %d, count: %d]",
+            startAtIndex,
+            count
+        );
+        uint16 index = startAtIndex;
+        uint16 checked = 0; // Counter to track the number of checked indices
+        uint16 foundUnset = 0; // Counter for the number of unset bits found
+
+        while (foundUnset < count && checked < rollover) {
+            if (!_revealedTokens.get(index)) {
+                foundUnset++;
+                console.log(
+                    "foundUnset [index: %d, index: %d]",
+                    foundUnset,
+                    index
+                );
+                if (foundUnset == count) {
+                    // Return the current index if the required number of unset bits is found
+                    return index;
+                }
+            }
+            index++;
+            checked++;
+            // Roll over the index back to zero if it reaches or exceeds startingSize
+            if (index >= rollover) {
+                index = 0;
+            }
+        }
+        // If not enough unset bits are found, revert
+        if (foundUnset < count) {
+            revert NotEnoughUnsetBits();
+        }
+        // This line is technically unreachable, but included for completeness
+        return index;
+    }
+
     error BlockMustBeInFuture();
-    function reveal(uint256 salt, uint16 reveals) external onlyRoles(REVEALER) {
+    error SizeCannotDecrease();
+    uint16 private highestTotalAvailableArt;
+    function reveal(
+        uint256 salt,
+        uint16 reveals,
+        uint16 totalAvailableArt
+    ) external onlyRoles(REVEALER) {
+        // Check if the new totalAvailableArt is less than the highest value seen
+        if (totalAvailableArt < highestTotalAvailableArt) {
+            revert SizeCannotDecrease();
+        }
+
+        // Update highestTotalAvailableArt if the new value is higher
+        if (totalAvailableArt > highestTotalAvailableArt) {
+            highestTotalAvailableArt = totalAvailableArt;
+        }
+        uint16 revealSetSize = totalAvailableArt -
+            (startingSize - unrevealedTokensRemaining);
+        console.log(
+            "[revealSetSize: %d, reveals: %d, totalAvailableArt: %d]",
+            revealSetSize,
+            reveals,
+            totalAvailableArt
+        );
         Revealed memory revealed;
         revealed.salt = salt;
         revealed.revealedCount = reveals;
@@ -175,14 +259,17 @@ contract FairReveal is OwnableRoles, ITokenURIGenerator {
 
         uint16 revealedCount = 0;
         // use hash to pick a start index mod unrevealedTokensRemaining
-        uint16 startAtIndex = findFirstUnset(
-            uint16(revealed.seed % uint256(startingSize))
+        uint16 startAtIndex = findFirstUnsetCounting(
+            0,
+            uint16(revealed.seed % uint256(revealSetSize)),
+            totalAvailableArt
         );
         _revealed.push(revealed);
         while (revealedCount < reveals) {
             uint16 endAtIndex = findFirstSetNoRollover(
-                startAtIndex,
-                reveals - revealedCount
+                startAtIndex + 1,
+                reveals - revealedCount,
+                totalAvailableArt
             );
             uint16 length = endAtIndex - startAtIndex;
             // length cannot be > reveals - revealed
@@ -196,9 +283,11 @@ contract FairReveal is OwnableRoles, ITokenURIGenerator {
             revealedCount += length;
             if (revealedCount < reveals) {
                 startAtIndex = findFirstUnset(
-                    endAtIndex >= startingSize - 1 ? 0 : endAtIndex
+                    endAtIndex > totalAvailableArt ? 0 : endAtIndex,
+                    totalAvailableArt
                 );
             }
+            console.log("[revealedCount: %d]", revealedCount);
         }
 
         if (address(emitable) != address(0)) {
--- test/FairReveal.t.sol
+++ test/FairReveal.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {LibString} from "solady/utils/LibString.sol";
 import {FairReveal} from "../src/FairReveal.sol";
 
@@ -18,11 +19,84 @@ contract FairRevealTest is Test {
         );
     }
 
+    struct RevealData {
+        uint256 size;
+        uint256 totalSize;
+        uint16[] reveals;
+        uint16[] availables;
+        uint16 minTotalAvailableSize;
+        uint16 maxTotalAvailableSize;
+        uint16 revealEventCount;
+        uint16 revealCount;
+    }
+    function generateRevealCheckpoint()
+        internal
+        returns (uint16[] memory reveals, uint16[] memory availables)
+    {
+        RevealData memory data = RevealData({
+            size: 8 +
+                (uint256(keccak256(abi.encodePacked(block.timestamp))) % 24),
+            totalSize: 0, // Initialize to 0, will be set properly below
+            reveals: new uint16[](0), // Temporary initialization
+            availables: new uint16[](0), // Temporary initialization
+            minTotalAvailableSize: 1,
+            maxTotalAvailableSize: 0, // Will be set properly below
+            revealEventCount: 0,
+            revealCount: 0
+        });
+
+        // Create a random integer i between 8 and 32.
+        data.totalSize =
+            data.size *
+            (8 + (uint256(keccak256(abi.encodePacked(block.timestamp))) % 24));
+        data.reveals = new uint16[](data.size);
+        data.availables = new uint16[](data.size);
+        data.maxTotalAvailableSize = uint16(data.totalSize);
+
+        while (data.revealCount < data.size) {
+            uint16 revealSize = 1 +
+                (uint16(
+                    keccak256(
+                        abi.encodePacked(
+                            block.timestamp,
+                            data.revealCount,
+                            data.totalSize
+                        )
+                    )
+                ) % (data.maxTotalAvailableSize - data.availables));
+            uint16 availableSize = 1 +
+                (uint16(
+                    keccak256(
+                        abi.encodePacked(
+                            block.timestamp,
+                            data.revealCount,
+                            data.totalSize
+                        )
+                    )
+                ) % (data.maxTotalAvailableSize - data.minTotalAvailableSize));
+            data.reveals[data.revealCount] = revealSize;
+            data.availables[data.revealCount] = availableSize;
+            data.revealCount++;
+            data.minTotalAvailableSize += revealSize;
+        }
+    }
+
+    function fixtureRevealCheckpoints()
+        public
+        returns (uint16[] memory reveals, uint16[] memory availables)
+    {
+        // Create a random integer i between 8 and 32. Multiple that integer by a random integer between 8 and 32.
+        // This will give us a random integer between 64 and 1024 and is the size of the collection to reveal.
+        // For the size of the collection take a random chunk between 1 and the size of the collection and save
+        // as revealCount and then take a second randomn chunk between 1 and the size of the collection to use as
+        // totalAvailableAmount. Continue generating reveal chunks until total
+    }
+
     function test_Reveal1() public {
-        fairReveal.reveal(0, 4);
+        fairReveal.reveal(0, 4, 8);
         vm.prevrandao(bytes32(uint256(0)));
         (uint256 tokenId, uint256 salt) = fairReveal.resolveTokenId(0);
-        assertEq(tokenId, 5);
+        assertEq(tokenId, 4);
         uint256 saltedTokenId = uint256(
             keccak256(abi.encodePacked(tokenId, salt))
         );
@@ -36,16 +110,23 @@ contract FairRevealTest is Test {
             fairReveal.tokenURI(4),
             LibString.concat("revealed://", saltedTokenId.toString())
         );
-        assertEq(tokenId, 0);
+        assertEq(tokenId, 7);
 
         assertEq(fairReveal.tokenURI(5), "unrevealed://5");
     }
 
+    function test_RevealCannotRemoveSize() public {
+        fairReveal.reveal(0, 2, 8);
+        vm.prevrandao(bytes32(uint256(0)));
+        vm.expectRevert(FairReveal.SizeCannotDecrease.selector);
+        fairReveal.reveal(0, 2, 6);
+    }
+
     function test_RevealFull() public {
         vm.prevrandao(bytes32(uint256(0)));
-        fairReveal.reveal(0, 8);
+        fairReveal.reveal(0, 8, 8);
         (uint256 tokenId, uint256 salt) = fairReveal.resolveTokenId(0);
-        assertEq(tokenId, 5);
+        assertEq(tokenId, 4);
         uint256 saltedTokenId = uint256(
             keccak256(abi.encodePacked(tokenId, salt))
         );
@@ -54,7 +135,7 @@ contract FairRevealTest is Test {
             LibString.concat("revealed://", saltedTokenId.toString())
         );
         (tokenId, salt) = fairReveal.resolveTokenId(7);
-        assertEq(tokenId, 4);
+        assertEq(tokenId, 3);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
         assertEq(
             fairReveal.tokenURI(8),
@@ -64,21 +145,89 @@ contract FairRevealTest is Test {
 
     function test_RevealLayers() public {
         vm.prevrandao(bytes32(uint256(0)));
-        fairReveal.reveal(1, 2);
+        fairReveal.reveal(1, 2, 8);
         vm.prevrandao(bytes32(uint256(0)));
-        fairReveal.reveal(2, 2);
+        fairReveal.reveal(2, 2, 8);
         vm.prevrandao(bytes32(uint256(5)));
-        fairReveal.reveal(3, 2);
+        fairReveal.reveal(3, 2, 8);
         vm.prevrandao(bytes32(uint256(0)));
-        fairReveal.reveal(4, 2);
-        // [startAtIndex: 1, length: 2]
-        // [startAtIndex: 3, length: 2]
-        // [startAtIndex: 0, length: 1]
-        // [startAtIndex: 5, length: 1]
-        // [startAtIndex: 6, length: 2]
+        fairReveal.reveal(4, 2, 8);
+
         uint256 saltedTokenId;
         uint256 tokenId;
         uint256 salt;
+        (tokenId, salt) = fairReveal.resolveTokenId(0);
+        assertEq(tokenId, 0);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(1),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+
+        (tokenId, salt) = fairReveal.resolveTokenId(1);
+        assertEq(tokenId, 1);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(2),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+
+        (tokenId, salt) = fairReveal.resolveTokenId(2);
+        assertEq(tokenId, 6);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(3),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+
+        (tokenId, salt) = fairReveal.resolveTokenId(3);
+        assertEq(tokenId, 7);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(4),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+
+        (tokenId, salt) = fairReveal.resolveTokenId(4);
+        assertEq(tokenId, 3);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(5),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+
+        (tokenId, salt) = fairReveal.resolveTokenId(5);
+        assertEq(tokenId, 4);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(6),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+
+        (tokenId, salt) = fairReveal.resolveTokenId(6);
+        assertEq(tokenId, 5);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(7),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+
+        (tokenId, salt) = fairReveal.resolveTokenId(7);
+        assertEq(tokenId, 2);
+        saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
+        assertEq(
+            fairReveal.tokenURI(8),
+            LibString.concat("revealed://", saltedTokenId.toString())
+        );
+    }
+
+    function test_RollingReveal() public {
+        vm.prevrandao(bytes32(uint256(5)));
+        fairReveal.reveal(0, 2, 4);
+        uint256 saltedTokenId;
+        uint256 tokenId;
+        uint256 salt;
+
         (tokenId, salt) = fairReveal.resolveTokenId(0);
         assertEq(tokenId, 1);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
@@ -95,8 +244,11 @@ contract FairRevealTest is Test {
             LibString.concat("revealed://", saltedTokenId.toString())
         );
 
+        assertEq(fairReveal.tokenURI(3), "unrevealed://3");
+
+        fairReveal.reveal(0, 2, 4);
         (tokenId, salt) = fairReveal.resolveTokenId(2);
-        assertEq(tokenId, 3);
+        assertEq(tokenId, 0);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
         assertEq(
             fairReveal.tokenURI(3),
@@ -104,37 +256,41 @@ contract FairRevealTest is Test {
         );
 
         (tokenId, salt) = fairReveal.resolveTokenId(3);
-        assertEq(tokenId, 4);
+        assertEq(tokenId, 3);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
         assertEq(
             fairReveal.tokenURI(4),
             LibString.concat("revealed://", saltedTokenId.toString())
         );
 
+        vm.prevrandao(bytes32(uint256(3)));
+        fairReveal.reveal(0, 1, 8);
+
         (tokenId, salt) = fairReveal.resolveTokenId(4);
-        assertEq(tokenId, 0);
+        assertEq(tokenId, 6);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
         assertEq(
             fairReveal.tokenURI(5),
             LibString.concat("revealed://", saltedTokenId.toString())
         );
 
+        vm.prevrandao(bytes32(uint256(4)));
+        fairReveal.reveal(0, 3, 8);
+
         (tokenId, salt) = fairReveal.resolveTokenId(5);
-        assertEq(tokenId, 5);
+        assertEq(tokenId, 4);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
         assertEq(
             fairReveal.tokenURI(6),
             LibString.concat("revealed://", saltedTokenId.toString())
         );
-
         (tokenId, salt) = fairReveal.resolveTokenId(6);
-        assertEq(tokenId, 6);
+        assertEq(tokenId, 5);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
         assertEq(
             fairReveal.tokenURI(7),
             LibString.concat("revealed://", saltedTokenId.toString())
         );
-
         (tokenId, salt) = fairReveal.resolveTokenId(7);
         assertEq(tokenId, 7);
         saltedTokenId = uint256(keccak256(abi.encodePacked(tokenId, salt)));
